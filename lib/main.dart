c561290ce4ae91696e7746ed28b5dbbf675ecb74
Shahadan Bin Saad <shahadansaad@gmail.com>
2020-02-17 23:17:24 +0800

    without comma

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Bismillah!'));
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(title: Text('Bismillah!'),),
+      body: Text('Dengan Nama Allah Yang Maha Pemurah Lagi Maha Mengasihani')
+    ));
   }
 }
