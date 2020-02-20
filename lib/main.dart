1435ba6d5118c7c7767ff2c98cc84369b12add54
Shahadan Bin Saad <shahadansaad@gmail.com>
2020-02-20 08:03:36 +0800

    final result lab3 - https://raw.githubusercontent.com/hanafiah/flutter_lab/lab3-final/lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,41 @@
-// Flutter code sample for Expanded
-
-// This example shows how to use an [Expanded] widget in a [Column] so that
-// it's middle child, a [Container] here, expands to fill the space.
-//
-// ![This results in two thin blue boxes with a larger amber box in between.](https://flutter.github.io/assets-for-api-docs/assets/widgets/expanded_column.png)
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      home: MyStatelessWidget(),
-    );
-  }
-}
-
-/// This is the stateless widget that the main application instantiates.
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Expanded Column Sample with Exercise 2'),
-      ),
-      body: Center(
-        child:Column(
+      title: 'Flutter Lab 3',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Lab 3'),
+        ),
+        body: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
+              Column(
+                children: <Widget>[
                   Icon(Icons.phone),
-                  Container(
-                      child:Text(
-                          "CALL"
-                      )
-                  )
+                  Container(child: Text('CALL'))
                 ],
-        )
-//          child:Row(
-//            children: <Widget>[
-//              Column(
-//                children: <Widget>[
-//                  Icon(Icons.phone),
-//                  Container(
-//                      child:Text(
-//                          "CALL"
-//                      )
-//                  )
-//                ],
-//              ),
-//              Column(
-//                children: <Widget>[
-//                  Icon(Icons.navigation),
-//                  Container(
-//                      child:Text(
-//                          "ROUTE"
-//                      )
-//                  )
-//                ],
-//              ),
-//              Column(
-//                children: <Widget>[
-//                  Icon(Icons.share),
-//                  Container(
-//                      child:Text(
-//                          "SHARE"
-//                      )
-//                  )
-//                ],
-//              ),
-//            ],
-//          )
-//        child: Text(
-//          "Bismillah",
-//          textDirection: TextDirection.ltr,
-//        )
-//        child: Column(
-//          children: <Widget>[
-//            Container(
-//              color: Colors.blue,
-//              height: 100,
-//              width: 100,
-//            ),
-//            Expanded(
-//              child: Container(
-//                color: Colors.amber,
-//                width: 100,
-//              ),
-//            ),
-//            Container(
-//              color: Colors.blue,
-//              height: 100,
-//              width: 100,
-//            ),
-//          ],
-//        ),
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(Icons.navigation),
+                  Container(child: Text('ROUTE'))
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(Icons.share),
+                  Container(child: Text('SHARE'))
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
